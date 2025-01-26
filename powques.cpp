1d743a4b0045159cd04c0927b01536d44454fb8f
Kaushik Dutt <kkdutt99@gmail.com>
2025-01-26 23:20:12 +0530

    Add files via upload

---
+++ powques.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+class solution{
+    public:
+        double myPow(double x, double y){
+            int result=1;
+            
+        }
+
+};
+int main(){
+
+    return 0;
+}
